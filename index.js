e33858114f7dc9f62e99a2a9e6fd7ecd954e8418
NatePennebaker <npenne1@aol.com>
2023-01-12 11:41:21 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki']
+const events = ['surprise']
+
+function writeCards(names, events){
+    let thankYous =[]
+    for (let i = 0; i < names.length; i++) {
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+}
+    return thankYous;
+}
+
+function countDown(){
+    let down = 11
+    while (down > 0) {
+        console.log(--down);
+}
+}
